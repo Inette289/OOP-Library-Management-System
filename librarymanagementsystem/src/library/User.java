aafe393acf943d57e9e5aabc4f7b8734499498df
Inette <inette.badenhorst.ib@gmail.com>
2026-01-08 20:58:21 +0200

    Add files via upload

---
+++ librarymanagementsystem/src/library/User.java
@@ -0,0 +1,19 @@
+package library;
+
+public class User {
+ private final int userId;
+    private final String name;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() { return userId; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return userId + " | " + name;
+    }
+}
